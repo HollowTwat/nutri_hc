42435f10aba825fe4661315143d7b2dd4aa26880
selivants <selivants@gmail.com>
2025-02-03 19:14:03 +0300

    testcommit

---
+++ day2.py
@@ -0,0 +1,26 @@
+import asyncio
+import aiogram
+import random
+import os
+import logging
+from aiogram import Bot, Dispatcher, types
+import openai
+import asyncio
+import logging
+import sys
+from aiogram import Bot, Dispatcher, html, Router, BaseMiddleware, types
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.filters.state import StateFilter
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, FSInputFile, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InputMediaPhoto, InputMediaVideo
+from openai import AsyncOpenAI, OpenAI
+
+import shelve
+import json
+
+from functions import *
+from functions2 import *
