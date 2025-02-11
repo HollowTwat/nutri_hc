614022a4f00f9ab08411eab718465281b7190957
selivants <selivants@gmail.com>
2025-02-12 01:06:36 +0300

    week3fix2

--- day20.py
+++ day20.py
@@ -47,7 +47,8 @@ async def process_l20_step_1(callback_query, state):
     )
     await callback_query.message.answer(
         "А ещё мне важно знать, как тебе программа этих трех недель. С какой вероятностью от 1 до 10 ты порекомендуешь её друзьям?",
-        reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="1", callback_data="1")],[InlineKeyboardButton(text="2", callback_data="2")],[InlineKeyboardButton(text="3", callback_data="3")],[InlineKeyboardButton(text="4", callback_data="4")],[InlineKeyboardButton(text="5", callback_data="5")],[InlineKeyboardButton(text="6", callback_data="6")],[InlineKeyboardButton(text="7", callback_data="7")],[InlineKeyboardButton(text="8", callback_data="8")],[InlineKeyboardButton(text="9", callback_data="9")],[InlineKeyboardButton(text="10", callback_data="10")]])
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="1", callback_data="1"),InlineKeyboardButton(text="2", callback_data="2"),InlineKeyboardButton(text="3", callback_data="3"),InlineKeyboardButton(text="4", callback_data="4"),InlineKeyboardButton(text="5", callback_data="5")],
+                                                           [InlineKeyboardButton(text="6", callback_data="6"),InlineKeyboardButton(text="7", callback_data="7"),InlineKeyboardButton(text="8", callback_data="8"),InlineKeyboardButton(text="9", callback_data="9"),InlineKeyboardButton(text="10", callback_data="10")]])
     )
 
 async def process_l20_step_2(callback_query, state):
