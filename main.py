98e94cae64fede7bfc1a348fd53daa09608f63bf
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2025-03-26 23:47:40 +0300

    testing_state-setting_shiiii

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from aiogram.filters import CommandStart, Command
 from aiogram.filters.state import StateFilter
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.base import StorageKey
 from aiogram.types import Message, FSInputFile, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InputMediaPhoto, InputMediaVideo
 from openai import AsyncOpenAI, OpenAI
 import shelve
@@ -2067,7 +2068,9 @@ async def set_user_state(message: types.Message):
     # Initialize FSMContext for the target user
     # ctx = FSMContext(storage=dp.storage, user_id=464682207, chat_id=464682207)
     # await ctx.set_state(UserState.recognition)
-    await dp.current_state(user=464682207, chat=464682207).set_state(UserState.recognition)
+    storage_key = StorageKey(bot.id, 464682207, 464682207)
+    fsm_context = FSMContext(storage=dp.storage, key=storage_key)
+    await fsm_context.set_state(UserState.recognition)
     await message.answer("set user 464682207 to recogni")
 
 @router.message(Command("upload_image"))
