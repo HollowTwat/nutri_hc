d7744dd485cda28f8b14779af9d6e2530997864d
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2025-06-19 00:20:19 +0300

    fix

--- main.py
+++ main.py
@@ -344,7 +344,7 @@ async def main_process_votes(callback_query: CallbackQuery, state: FSMContext):
     await callback_query.answer()
     vote_num = callback_query.data.split("_")[1]
     succesfull_count = await get_user_vote(callback_query.from_user.id, vote_num)
-    asyncio.create_task(log_bot_response(f"user voted {vote_num}, successresponse: {succesfull_count}"))
+    asyncio.create_task(log_bot_response(f"user voted {vote_num}, successresponse: {succesfull_count}", callback_query.from_user.id))
     await callback_query.message.edit_text("Спасибо за участие в опросе", reply_markup=None)
 
 ################## Layover Layover Layover Layover Layover Layover Layover Layover Layover Layover Layover Layover Layover Layover Layover ##################
